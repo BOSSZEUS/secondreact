76ba67a0d86754df2f86d881af8c210a0d0d5208
Quinton Fults <coding.banjo@gmail.com>
2020-03-18 10:32:05 -0700

    added component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Heading from './components/Heading'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Heading />
     </div>
   )
 }
